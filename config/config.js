5c8fa0a02bbe7e0d8c9488af8686b93adadb69ad
Markus Limbacher <markus.limbacher@diconium.com>
2020-06-19 11:22:27 +0200

    added config

---
+++ config/config.js
@@ -0,0 +1,11 @@
+var config = {};
+
+config.tensorflow = {
+	modelPath: './model',
+	epochs: '20',
+	batchSize: '128',
+	imageHeight: 28,
+	imageWidth: 28,
+	imageHeaderSize: 16,
+	MNISTHost: 'https://storage.googleapis.com/cvdf-datasets/mnist/'
+}

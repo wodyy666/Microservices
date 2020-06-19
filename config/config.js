f1d3c2f753166f7a578bf46f09f7584800af72b4
Markus Limbacher <markus.limbacher@diconium.com>
2020-06-19 11:53:57 +0200

    rewrote model.js

--- config/config.js
+++ config/config.js
@@ -8,4 +8,65 @@ config.tensorflow = {
 	imageWidth: 28,
 	imageHeaderSize: 16,
 	MNISTHost: 'https://storage.googleapis.com/cvdf-datasets/mnist/'
+
 }
+
+config.tensorflow.model = {
+	layers: [
+		{
+			type: 'conv2d',
+			inputShape: [28, 28, 1],
+			filters: 32,
+			kernelSize: 3,
+			activation: 'relu'
+		},
+		{
+			type: 'conv2d',
+			filters: 32,
+			kernelSize: 3,
+			activation: 'relu'
+		},
+		{
+			type: 'maxPooling2d',
+			poolSize: [2, 2]
+		},
+		{
+			type: 'conv2d',
+			filters: 64,
+			kernelSize: 3,
+			activation: 'relu'
+		},
+		{
+			type: 'conv2d',
+			filters: 64,
+			kernelSize: 3,
+			activation: 'relu'
+		},
+		{
+			type: 'maxPooling2d',
+			poolSize: [2, 2]
+		},
+		{
+			type: 'flatten',
+			dropout: 0.25
+		},
+		{
+			type: 'dense',
+			units: 512,
+			activation: 'relu',
+			dropout: 0.5
+		},
+		{
+			type: 'result',
+			units: 10,
+			activation: 'softmax'
+		}
+	],
+	compileOptions: {
+		optimizer: 'rmsprop',
+		loss: 'categoricalCrossentropy',
+		metrics: ['accuracy']
+	}
+}
+
+module.exports = config;
--- helpers/model.js
+++ helpers/model.js
@@ -1,57 +1,32 @@
-/**
- * @license
- * Copyright 2018 Google LLC. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * =============================================================================
- */
-
 const tf = require('@tensorflow/tfjs-node/dist/index');
+const { tensorflow } = require('../config/config.js');
 
 const model = tf.sequential();
-model.add(tf.layers.conv2d({
-	inputShape: [28, 28, 1],
-	filters: 32,
-	kernelSize: 3,
-	activation: 'relu',
-}));
-model.add(tf.layers.conv2d({
-	filters: 32,
-	kernelSize: 3,
-	activation: 'relu',
-}));
-model.add(tf.layers.maxPooling2d({poolSize: [2, 2]}));
-model.add(tf.layers.conv2d({
-	filters: 64,
-	kernelSize: 3,
-	activation: 'relu',
-}));
-model.add(tf.layers.conv2d({
-	filters: 64,
-	kernelSize: 3,
-	activation: 'relu',
-}));
-model.add(tf.layers.maxPooling2d({poolSize: [2, 2]}));
-model.add(tf.layers.flatten());
-model.add(tf.layers.dropout({rate: 0.25}));
-model.add(tf.layers.dense({units: 512, activation: 'relu'}));
-model.add(tf.layers.dropout({rate: 0.5}));
-model.add(tf.layers.dense({units: 10, activation: 'softmax'}));
 
-const optimizer = 'rmsprop';
-model.compile({
-	optimizer: optimizer,
-	loss: 'categoricalCrossentropy',
-	metrics: ['accuracy'],
+// adds each layer specified in the config to the model
+tensorflow.model.layers.forEach(layer => {
+	switch (layer.type) {
+		case "conv2d":
+			model.add(tf.layers.conv2d(layer));
+			break;
+		case "maxPooling2d":
+			model.add(tf.layers.maxPooling2d(layer));
+			break;
+		case "flatten":
+			model.add(tf.layers.flatten());
+			model.add(tf.layers.dropout(layer.dropout));
+			break;
+		case "dense":
+			model.add(tf.layers.dense(layer));
+			model.add(tf.layers.dropout(layer.dropout));
+			break;
+		case "result":
+			model.add(tf.layers.dense(layer));
+			break;
+	}
 });
+
+// compile the model with all specified layers added
+model.compile(tensorflow.model.compileOptions);
 
 module.exports = model;

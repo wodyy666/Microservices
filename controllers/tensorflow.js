bdefa11b874e81aac0f4ca10e0815d0de6c23de7
Markus Limbacher <markus.limbacher@diconium.com>
2020-06-23 09:39:49 +0200

    improved handlePredict

--- controllers/tensorflow.js
+++ controllers/tensorflow.js
@@ -1,8 +1,13 @@
+const runner = require('../helpers/runner');
+
 function handleTrainModel(req, res) {
 	res.sendStatus(200);
 }
 
 function handlePredict(req, res) {
+	let image = req.body.image;
+	console.log(image);
+	runner.runModel(image);
 	res.sendStatus(200);
 }
 

c0c20cd46cf8d605666d1a1fd448e7f183505666
Markus Limbacher <markus.limbacher@diconium.com>
2020-06-26 16:46:11 +0200

    increased canvas size

--- public/script.js
+++ public/script.js
@@ -5,7 +5,7 @@ var canvas, ctx, flag = false,
 	currY = 0,
 	dot_flag = false,
 	color = 'black',
-	lineWidth = 2,
+	lineWidth = 5,
 	autoUpdate = false;
 
 function init() {
@@ -22,7 +22,7 @@ function init() {
 	}, false);
 	canvas.addEventListener('mouseup', function(e) {
 		findxy('up', e);
-		if(autoUpdate) {
+		if (autoUpdate) {
 			predict();
 		}
 	}, false);
@@ -30,7 +30,7 @@ function init() {
 		findxy('out', e);
 	}, false);
 
-	autoUpdate = document.getElementById("checkbox").checked;
+	autoUpdate = document.getElementById('checkbox').checked;
 }
 
 function setAutoUpdate(checkbox) {
@@ -51,22 +51,33 @@ function erase() {
 	ctx.clearRect(0, 0, w, h);
 }
 
-function prepareImageArray() {
-	return new Promise(resolve => {
+async function prepareImageArray() {
+	return await new Promise(async resolve => {
 		var array = new Float32Array(28 * 28);
-		for (var y = 0; y < 28; y++) {
-
-			for (var x = 0; x < 28; x++) {
-				if (ctx.getImageData(x, y, 1, 1).
+		for (var y = 0; y < 140; y += 5) {
+			for (var x = 0; x < 140; x += 5) {
+				var tempArray = ctx.getImageData(x, y, 5, 5).
 					data.
 					toString().
-					split(',')[3] != 255) {
-					array[(y * 28) + x] = 0;
-				} else {
-					array[(y * 28) + x] = 1;
+					split(',');
+				var sum = await new Promise(resolve => {
+					var sum = 0;
+					for (var i = 0; i < 25; i++) {
+						sum += parseInt(tempArray[3 + (i * 4)]);
+					}
+					resolve(sum);
+				});
+
+				if(sum > 2550) {
+					sum = 255;
 				}
+				else {
+					sum = 0;
+				}
+				array[((y / 5) * 28) + (x / 5)] = sum;
 			}
 		}
+
 		resolve(array);
 	});
 }
@@ -75,7 +86,7 @@ async function predict() {
 	let array = await prepareImageArray();
 	sendImage(array);
 
-	document.getElementById("resultDiv").style.display = "block";
+	document.getElementById('resultDiv').style.display = 'block';
 }
 
 function sendImage(array) {
@@ -85,23 +96,24 @@ function sendImage(array) {
 	let body = json;
 	console.log(body);
 
-	xhr.addEventListener("readystatechange", function() {
-		if(this.readyState === 4) {
+	xhr.addEventListener('readystatechange', function() {
+		if (this.readyState === 4) {
 			var array = JSON.parse(this.responseText);
 			setLabels(array);
 		}
 	});
 
-	xhr.open("POST", "/predict");
-	xhr.setRequestHeader("Content-Type", "application/json");
+	xhr.open('POST', '/predict');
+	xhr.setRequestHeader('Content-Type', 'application/json');
 	xhr.send(body);
 }
 
-function setLabels(array){
+function setLabels(array) {
 	array.forEach((item, index) => {
 		var percent = (item * 100).toFixed(2);
-		document.getElementById("result" + index).value = percent;
-		document.getElementById("result" + index + "lbl").innerHTML = percent + "%";
+		document.getElementById('result' + index).value = percent;
+		document.getElementById('result' + index + 'lbl').innerHTML = percent +
+			'%';
 	});
 }
 
